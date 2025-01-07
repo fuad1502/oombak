e3d026d7c3447e9f986e97f52c7f582b2040c785
Fuad Ismail <fuad1502@gmail.com>
2025-01-07 19:37:48 +0700

    ombak-tui/bitve_str: add parse method.

--- ombak-tui/src/utils/bitvec_str.rs
+++ ombak-tui/src/utils/bitvec_str.rs
@@ -1,4 +1,4 @@
-use bitvec::prelude::BitVec;
+use bitvec::{order::Lsb0, prelude::BitVec};
 
 use crate::components::models;
 
@@ -17,6 +17,18 @@ pub enum Format {
 pub fn from(bit_vec: &BitVec<u32>, option: &Option) -> String {
     match option.format {
         Format::Binary => binary(bit_vec, option.width, option.twos_complement),
+    }
+}
+
+pub fn parse(value: &str) -> Result<BitVec<u32>, String> {
+    let chars: Vec<char> = value.chars().collect();
+    if chars.len() == 0 {
+        return Err("cannot parse empty value".to_string());
+    }
+    if chars[0] == '0' && chars.len() > 1 {
+        parse_non_decimal(&chars)
+    } else {
+        parse_decimal(&chars)
     }
 }
 
@@ -54,4 +66,27 @@ fn get_resized_bitvec(bit_vec: &BitVec<u32>, width: usize, twos_complement: bool
     };
     bit_vec.resize(width, fills);
     bit_vec
+}
+
+fn parse_non_decimal(chars: &[char]) -> Result<BitVec<u32>, String> {
+    match chars[1] {
+        'b' => parse_binary(&chars[2..]),
+        c => Err(format!("unknown radix identifier '0{c}'")),
+    }
+}
+
+fn parse_binary(chars: &[char]) -> Result<BitVec<u32>, String> {
+    let mut result = bitvec::bitvec![u32, Lsb0;];
+    for c in chars {
+        match c {
+            '0' => result.push(false),
+            '1' => result.push(true),
+            _ => return Err("binary value can only contain 1's and 0's".to_string()),
+        }
+    }
+    Ok(result)
+}
+
+fn parse_decimal(_chars: &[char]) -> Result<BitVec<u32>, String> {
+    Err("parsing decimal value not yet supported".to_string())
 }
