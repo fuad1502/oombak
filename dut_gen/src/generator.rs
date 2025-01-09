b61ad6d7265c185cb27ea4a489d3b9e99a8014cc
Fuad Ismail <fuad1502@gmail.com>
2025-01-09 18:11:40 +0700

    dut_gen: add getters.cpp and setters.cpp generator

--- dut_gen/src/generator.rs
+++ dut_gen/src/generator.rs
@@ -6,7 +6,7 @@ use std::{
 
 use crate::{error::DutGenResult, parser};
 
-macro_rules! generate_lines_from_template {
+macro_rules! generate_lines_from_name_template {
     ($template:expr, $signals:expr) => {{
         let signals: Box<dyn Iterator<Item = &crate::parser::Signal>> = Box::new($signals);
         signals
@@ -14,6 +14,71 @@ macro_rules! generate_lines_from_template {
             .collect::<Vec<String>>()
             .join("\n")
     }};
+}
+
+macro_rules! generate_lines_from_name_width_template {
+    ($template:expr, $signals:expr) => {{
+        let signals: Box<dyn Iterator<Item = &crate::parser::Signal>> = Box::new($signals);
+        signals
+            .map(|s| format!($template, s.name, s.width))
+            .collect::<Vec<String>>()
+            .join("\n")
+    }};
+}
+
+macro_rules! single_bit_getter_template {
+    () => {
+        concat!(
+            "pair<vector<uint32_t>, uint64_t> Dut::get_{0}(Dut *self) {{\n",
+            "    svBit out;\n",
+            "    self->vDut->v_sample_get_{0}(&out);\n",
+            "    return {{vector<uint32_t>(1, out), 1}};\n",
+            "}}\n"
+        )
+    };
+}
+
+macro_rules! single_bit_setter_template {
+    () => {
+        concat!(
+            "bool Dut::set_{0}(Dut *self, const vector<uint32_t> &words) {{\n",
+            "  if (words.size() > 0) {{\n",
+            "    self->vDut->v_sample_set_{0}(words[0]);\n",
+            "    return true;\n",
+            "  }}\n",
+            "  return false;\n",
+            "}}\n"
+        )
+    };
+}
+
+macro_rules! multi_bit_getter_template {
+    () => {
+        concat!(
+            "pair<vector<uint32_t>, uint64_t> Dut::get_{0}(Dut *self) {{\n",
+            "  int nBits = {1};\n",
+            "  svBitVecVal out[nBits / 32 + 1];\n",
+            "  self->vDut->v_sample_get_{0}(out);\n",
+            "  return {{Dut::get_words_vec_from(out, nBits), nBits}};\n",
+            "}}\n"
+        )
+    };
+}
+
+macro_rules! multi_bit_setter_template {
+    () => {
+        concat!(
+            "bool Dut::set_{0}(Dut *self, const vector<uint32_t> &words) {{\n",
+            "  int nBits = {1};\n",
+            "  svBitVecVal in[nBits / 32];\n",
+            "  if (Dut::set_from_words_vec(in, words, nBits)) {{\n",
+            "    self->vDut->v_sample_set_{0}(in);\n",
+            "    return true;\n",
+            "  }}\n",
+            "  return false;\n",
+            "}}\n",
+        )
+    };
 }
 
 pub fn generate(_sv_path: &Path, probe: &parser::Probe) -> DutGenResult<PathBuf> {
@@ -39,6 +104,8 @@ impl<'a> Generator<'a> {
         self.put_dut_bind_h()?;
         self.put_dut_cpp()?;
         self.put_dut_hpp()?;
+        self.put_getters_cpp()?;
+        self.put_setters_cpp()?;
         Ok(PathBuf::new())
     }
 
@@ -62,11 +129,11 @@ impl<'a> Generator<'a> {
 
     fn put_dut_cpp(&self) -> DutGenResult<()> {
         let content = include_str!("templates/dut.cpp.templated");
-        let setters = generate_lines_from_template!(
+        let setters = generate_lines_from_name_template!(
             "signalMapping[\"{0}\"].set = set_{0}",
             self.probe.signals.iter().filter(|s| s.set)
         );
-        let getters = generate_lines_from_template!(
+        let getters = generate_lines_from_name_template!(
             "signalMapping[\"{0}\"].get = get_{0}",
             self.probe.signals.iter().filter(|s| s.get)
         );
@@ -78,17 +145,53 @@ impl<'a> Generator<'a> {
 
     fn put_dut_hpp(&self) -> DutGenResult<()> {
         let content = include_str!("templates/dut.hpp.templated");
-        let setters = generate_lines_from_template!(
+        let setters = generate_lines_from_name_template!(
             "static bool set_{0}(Dut *self, const std::vector<uint32_t> &words);",
             self.probe.signals.iter().filter(|s| s.set)
         );
-        let getters = generate_lines_from_template!(
+        let getters = generate_lines_from_name_template!(
             "static std::pair<std::vector<uint32_t>, uint64_t> get_{0}(Dut *self);",
             self.probe.signals.iter().filter(|s| s.get)
         );
         let content = content.replace("// TEMPLATED: setters", &setters);
         let content = content.replace("// TEMPLATED: getters", &getters);
         self.put_file("dut.hpp", content.as_bytes())?;
+        Ok(())
+    }
+
+    fn put_getters_cpp(&self) -> DutGenResult<()> {
+        let content = include_str!("templates/getters.cpp.templated");
+        let single_bit_signals = self.probe.signals.iter().filter(|s| s.get && s.width == 1);
+        let multi_bit_signals = self.probe.signals.iter().filter(|s| s.get && s.width > 1);
+        let single_bit_getters =
+            generate_lines_from_name_template!(single_bit_getter_template!(), single_bit_signals);
+        let multi_bit_getters = generate_lines_from_name_width_template!(
+            multi_bit_getter_template!(),
+            multi_bit_signals
+        );
+        let content = content.replace(
+            "// TEMPLATED: getters",
+            &(single_bit_getters + "\n" + &multi_bit_getters),
+        );
+        self.put_file("getters.cpp", content.as_bytes())?;
+        Ok(())
+    }
+
+    fn put_setters_cpp(&self) -> DutGenResult<()> {
+        let content = include_str!("templates/setters.cpp.templated");
+        let single_bit_signals = self.probe.signals.iter().filter(|s| s.set && s.width == 1);
+        let multi_bit_signals = self.probe.signals.iter().filter(|s| s.set && s.width > 1);
+        let single_bit_setters =
+            generate_lines_from_name_template!(single_bit_setter_template!(), single_bit_signals);
+        let multi_bit_setters = generate_lines_from_name_width_template!(
+            multi_bit_setter_template!(),
+            multi_bit_signals
+        );
+        let content = content.replace(
+            "// TEMPLATED: setters",
+            &(single_bit_setters + "\n" + &multi_bit_setters),
+        );
+        self.put_file("setters.cpp", content.as_bytes())?;
         Ok(())
     }
 
