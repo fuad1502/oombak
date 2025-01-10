5d43bcf1894ef21d8b22da1229779101e4edb88c
Fuad Ismail <fuad1502@gmail.com>
2025-01-10 09:58:57 +0700

    dut_gen/parser: add module_name field to Probe and top_level field to Signal

--- dut_gen/src/parser.rs
+++ dut_gen/src/parser.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 
 pub struct Probe {
+    pub module_name: String,
     pub signals: Vec<Signal>,
 }
 
@@ -9,6 +10,7 @@ pub struct Signal {
     pub width: u64,
     pub get: bool,
     pub set: bool,
+    pub top_level: bool,
 }
 
 pub fn parse(_sv_path: &Path) -> Result<Probe, String> {
@@ -19,37 +21,46 @@ pub fn parse(_sv_path: &Path) -> Result<Probe, String> {
             width: 1,
             get: true,
             set: true,
+            top_level: true,
         },
         Signal {
             name: "rst_n".to_string(),
             width: 1,
             get: true,
             set: true,
+            top_level: true,
         },
         Signal {
             name: "in".to_string(),
             width: 6,
             get: true,
             set: true,
+            top_level: true,
         },
         Signal {
             name: "out".to_string(),
             width: 6,
             get: true,
             set: false,
+            top_level: true,
         },
         Signal {
             name: "sample_DOT_c".to_string(),
             width: 6,
             get: true,
             set: false,
+            top_level: false,
         },
         Signal {
             name: "sample_DOT_adder_inst_DOT_d".to_string(),
             width: 1,
             get: true,
             set: false,
+            top_level: false,
         },
     ];
-    Ok(Probe { signals })
+    Ok(Probe {
+        module_name: "sample".to_string(),
+        signals,
+    })
 }
