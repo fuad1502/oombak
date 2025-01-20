592fa6a0160274034d0ab48344cf84d1e37551ff
Fuad Ismail <fuad1502@gmail.com>
2025-01-20 17:54:24 +0700

    oombak_sim: add ModifyProbePoints request

--- oombak_sim/src/sim.rs
+++ oombak_sim/src/sim.rs
@@ -28,6 +28,7 @@ pub enum Request {
     Run(u64),
     SetSignal(String, BitVec<u32>),
     Load(PathBuf),
+    ModifyProbedPoints(ProbePointsModification),
     GetSimulationResult,
     Terminate,
 }
@@ -36,10 +37,16 @@ pub enum Response<'a> {
     RunResult(Result<u64, String>),
     SetSignalResult(Result<(), String>),
     LoadResult(Result<LoadedDut, String>),
+    ModifyProbedPointsResult(Result<LoadedDut, String>),
     SimulationResult(Result<&'a SimulationResult, String>),
 }
 
 pub use oombak_rs::parser::{InstanceNode, Signal, SignalType};
+
+pub struct ProbePointsModification {
+    to_add: Vec<String>,
+    to_remove: Vec<String>,
+}
 
 pub struct LoadedDut {
     pub root_node: InstanceNode,
@@ -75,6 +82,9 @@ impl Simulator {
                         server.serve_set_signal(&signal_name, &value)
                     }
                     Request::Load(sv_path) => server.serve_load(&sv_path),
+                    Request::ModifyProbedPoints(probe_points_modification) => {
+                        server.serve_modify_probe_points(&probe_points_modification)
+                    }
                     Request::GetSimulationResult => server.serve_simulation_result(),
                     Request::Terminate => break (Ok(())),
                 }
@@ -86,6 +96,7 @@ impl Simulator {
 struct RequestServer {
     dut: Option<Dut>,
     probe: Option<Probe>,
+    sv_path: Option<PathBuf>,
     listeners: Arc<RwLock<Listeners>>,
     simulation_time: u64,
     simulation_result: SimulationResult,
@@ -95,6 +106,7 @@ impl RequestServer {
     fn new(listeners: Arc<RwLock<Listeners>>) -> Self {
         Self {
             dut: None,
+            sv_path: None,
             probe: None,
             listeners,
             simulation_time: 0,
@@ -126,6 +138,14 @@ impl RequestServer {
         self.notify_listeners(response);
     }
 
+    fn serve_modify_probe_points(&mut self, probe_points_modification: &ProbePointsModification) {
+        let response = match self.modify_probe_points(probe_points_modification) {
+            Ok(loaded_dut) => Response::ModifyProbedPointsResult(Ok(loaded_dut)),
+            Err(e) => Response::ModifyProbedPointsResult(Err(e.to_string())),
+        };
+        self.notify_listeners(response);
+    }
+
     fn serve_simulation_result(&self) {
         let response = Response::SimulationResult(Ok(&self.simulation_result));
         self.notify_listeners(response);
@@ -133,22 +153,47 @@ impl RequestServer {
 
     fn load_dut(&mut self, sv_path: &Path) -> OombakSimResult<LoadedDut> {
         let (lib_path, probe) = oombak_gen::build(sv_path)?;
-        let probed_points = probe
-            .get_probed_points()
-            .iter()
-            .map(|p| p.path().to_string())
-            .collect();
-        let root_node = probe.root_node().clone();
+        let loaded_dut = LoadedDut::from(&probe);
+        self.dut = Some(Dut::new(lib_path.to_string_lossy().as_ref())?);
+        self.sv_path = Some(sv_path.to_path_buf());
         self.probe = Some(probe);
-        let lib_path = lib_path.to_str().unwrap();
-        let dut = Dut::new(lib_path)?;
-        self.dut = Some(dut);
+        self.reload_simulation_result()?;
+        Ok(loaded_dut)
+    }
+
+    fn modify_probe_points(
+        &mut self,
+        probe_points_modification: &ProbePointsModification,
+    ) -> OombakSimResult<LoadedDut> {
+        self.modify_probe(probe_points_modification)?;
+        let lib_path = self.rebuild_sv_path()?;
+        self.dut = Some(Dut::new(lib_path.to_string_lossy().as_ref())?);
+        self.reload_simulation_result()?;
+        Err(OombakSimError::DutNotLoaded)
+    }
+
+    fn modify_probe(
+        &mut self,
+        probe_points_modification: &ProbePointsModification,
+    ) -> OombakSimResult<()> {
+        let probe = self.probe.as_mut().ok_or(OombakSimError::DutNotLoaded)?;
+        for path in probe_points_modification.to_add.iter() {
+            probe.add_signal_to_probe(path)?;
+        }
+        Ok(())
+    }
+
+    fn rebuild_sv_path(&self) -> OombakSimResult<PathBuf> {
+        match (&self.sv_path, &self.probe) {
+            (Some(sv_path), Some(probe)) => Ok(oombak_gen::build_with_probe(sv_path, probe)?),
+            _ => Err(OombakSimError::DutNotLoaded),
+        }
+    }
+
+    fn reload_simulation_result(&mut self) -> OombakSimResult<()> {
         self.simulation_result = SimulationResult::default();
         self.load_signal_names_to_simulation_result()?;
-        Ok(LoadedDut {
-            root_node,
-            probed_points,
-        })
+        Ok(())
     }
 
     fn load_signal_names_to_simulation_result(&mut self) -> OombakSimResult<()> {
@@ -208,6 +253,21 @@ impl RequestServer {
     fn notify_listeners(&self, message: Response) {
         for listener in self.listeners.read().unwrap().iter() {
             listener.write().unwrap().on_receive_reponse(&message);
+        }
+    }
+}
+
+impl From<&Probe> for LoadedDut {
+    fn from(probe: &Probe) -> Self {
+        let probed_points = probe
+            .get_probed_points()
+            .iter()
+            .map(|p| p.path().to_string())
+            .collect();
+        let root_node = probe.root_node().clone();
+        LoadedDut {
+            probed_points,
+            root_node,
         }
     }
 }
