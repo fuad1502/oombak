3c9317422a22745fd651ebd1d310c66044cdaa52
Fuad Ismail <fuad1502@gmail.com>
2025-01-23 15:16:55 +0700

    oombak_tui/time_bar: pass render test

--- oombak_tui/src/widgets/time_bar.rs
+++ oombak_tui/src/widgets/time_bar.rs
@@ -1,4 +1,6 @@
-use ratatui::{buffer::Buffer, layout::Rect, style::Style, widgets::StatefulWidget};
+use ratatui::{
+    buffer::Buffer, layout::Rect, style::Style, style::Stylize, text::Line, widgets::StatefulWidget,
+};
 
 #[derive(Default)]
 pub struct TimeBar {
@@ -30,12 +32,72 @@ impl TimeBar {
 impl StatefulWidget for TimeBar {
     type State = TimeBarState;
 
-    fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {}
+    fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
+        state.set_viewport_length(area.width as usize);
+        let number_of_ticks = state.viewport_length / self.tick_period;
+        let floored_start_position = state.start_position / self.tick_period * self.tick_period;
+
+        let mut line_1 = String::new();
+        let mut line_2 = String::new();
+        for i in 0..number_of_ticks + 1 {
+            let time = floored_start_position + i * self.tick_period;
+            let segment = format!("╻{0:1$}", self.format(time), self.tick_period - 1);
+            line_1 += &segment;
+            let sub_ticks_left = usize::saturating_sub(self.tick_period, 2) / 2;
+            let sub_ticks_right = sub_ticks_left;
+            let middle_ticks = self.tick_period - sub_ticks_left - sub_ticks_right - 1;
+            let segment = format!(
+                "┻{0:┷<1$}{0:┻<2$}{0:┷<3$}",
+                "", sub_ticks_left, middle_ticks, sub_ticks_right
+            );
+            line_2 += &segment;
+        }
+
+        let skip_start = state.start_position - floored_start_position;
+        let line_1: String = line_1
+            .chars()
+            .skip(skip_start)
+            .take(state.viewport_length)
+            .collect();
+        let line_1 = Line::from(line_1);
+        let line_2: String = line_2
+            .chars()
+            .skip(skip_start)
+            .take(state.viewport_length)
+            .collect();
+        let line_2 = Line::from(line_2);
+
+        buf.set_line(0, 0, &line_1, area.width);
+        buf.set_line(0, 1, &line_2, area.width);
+
+        buf.set_style(
+            Rect::new(state.selected_position as u16, 0, 1, 2),
+            Style::default().on_red(),
+        );
+    }
+}
+
+impl TimeBar {
+    fn format(&self, time: usize) -> String {
+        format!("{} {}", time, self.time_unit)
+    }
+}
+
+impl std::fmt::Display for TimeUnit {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TimeUnit::Picoseconds => f.write_str("ps"),
+            TimeUnit::Nanoseconds => f.write_str("ns"),
+            TimeUnit::Miliseconds => f.write_str("ms"),
+        }
+    }
 }
 
 #[derive(Default)]
 pub struct TimeBarState {
     total_time: usize,
+    start_position: usize,
+    selected_position: usize,
     viewport_length: usize,
 }
 
