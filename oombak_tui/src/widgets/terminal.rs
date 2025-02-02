74dfbcb5c38fec2534f1c96a29dd93a018b90c3c
Fuad Ismail <fuad1502@gmail.com>
2025-02-02 14:37:26 +0700

    oombak_tui: refactor Terminal render function

--- oombak_tui/src/widgets/terminal.rs
+++ oombak_tui/src/widgets/terminal.rs
@@ -3,7 +3,7 @@ use ratatui::{
     layout::{Constraint, Layout, Rect},
     style::Stylize,
     text::{Line, Span},
-    widgets::{List, ListDirection, ListItem, ListState, Paragraph, StatefulWidget, Widget},
+    widgets::{List, ListDirection, ListItem, ListState, StatefulWidget, Widget},
 };
 
 use super::ScrollState;
@@ -30,32 +30,41 @@ impl StatefulWidget for Terminal {
 
     fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
         let chunks = Layout::vertical(vec![Constraint::Min(0), Constraint::Length(1)]).split(area);
+        Self::render_output_history(chunks[0], buf, state);
+        Self::render_command_line(chunks[1], buf, &mut state.command_line_state);
+    }
+}
+
+impl Terminal {
+    const HEADER: &'static str = " >>> ";
+
+    fn render_output_history(area: Rect, buf: &mut Buffer, state: &mut TerminalState) {
         let list_items = Self::new_list_items_from_output_history(&state.output_history);
         let list = List::new(list_items).direction(ListDirection::BottomToTop);
-        StatefulWidget::render(list, chunks[0], buf, &mut state.history_list_state);
+        StatefulWidget::render(list, area, buf, &mut state.history_list_state);
+    }
 
-        let state = &mut state.command_line_state;
+    fn render_command_line(area: Rect, buf: &mut Buffer, state: &mut CommandLineState) {
         state
             .scroll_state
-            .set_viewport_length(area.width as usize - 7);
+            .set_viewport_length(area.width as usize - (Self::HEADER.len() + 1) - 1);
+
         let start_idx = state.scroll_state.start_position();
-        let highlight_idx = state
-            .text
-            .char_indices()
-            .nth(state.cursor_position)
-            .unwrap_or((state.text.len(), ' '))
-            .0;
+        let highlight_idx =
+            get_utf8_index(&state.text, state.cursor_position).unwrap_or(state.text.len());
+
+        let highlight = state.text.get(highlight_idx..=highlight_idx).unwrap_or(" ");
+        let before_highlight = state.text.get(start_idx..highlight_idx).unwrap_or(" ");
+        let after_highlight = state.text.get(highlight_idx + 1..).unwrap_or(" ");
+
         let command_line = Line::from(vec![
-            Span::from(" >>> ").black().on_yellow(),
+            Span::from(Self::HEADER).black().on_yellow(),
             Span::from(" "),
-            Span::from(state.text.get(start_idx..highlight_idx).unwrap_or(" ")),
-            Span::from(state.text.get(highlight_idx..=highlight_idx).unwrap_or(" "))
-                .black()
-                .on_white(),
-            Span::from(state.text.get(highlight_idx + 1..).unwrap_or(" ")),
+            Span::from(before_highlight),
+            Span::from(highlight).black().on_white(),
+            Span::from(after_highlight),
         ]);
-        let paragraph = Paragraph::new(command_line).on_blue();
-        paragraph.render(chunks[1], buf);
+        command_line.on_blue().render(area, buf);
     }
 }
 
@@ -76,12 +85,7 @@ impl TerminalState {
 
     pub fn put(&mut self, c: char) {
         let state = &mut self.command_line_state;
-        let idx = state
-            .text
-            .char_indices()
-            .nth(state.cursor_position)
-            .unwrap_or((state.text.len(), ' '))
-            .0;
+        let idx = get_utf8_index(&state.text, state.cursor_position).unwrap_or(state.text.len());
         state.text.insert(idx, c);
         state.cursor_position += 1;
         state.scroll_state.set_content_length(state.text.len());
@@ -91,12 +95,7 @@ impl TerminalState {
     pub fn backspace(&mut self) {
         let state = &mut self.command_line_state;
         if state.cursor_position >= 1 {
-            let idx = state
-                .text
-                .char_indices()
-                .nth(state.cursor_position - 1)
-                .unwrap()
-                .0;
+            let idx = get_utf8_index(&state.text, state.cursor_position - 1).unwrap();
             state.text.remove(idx);
             state.cursor_position = state.cursor_position.saturating_sub(1);
             state.scroll_state.set_content_length(state.text.len());
@@ -136,6 +135,10 @@ impl Terminal {
             })
             .collect()
     }
+}
+
+fn get_utf8_index(s: &str, ch_idx: usize) -> Option<usize> {
+    s.char_indices().nth(ch_idx).map(|(i, _)| i)
 }
 
 #[cfg(test)]
