9d587a66dd76a5de5a9bdf854ef849fc298f4ae7
Fuad Ismail <fuad1502@gmail.com>
2025-06-05 23:06:55 +0700

    oombak_sim: add SetPeriodic request Payload

--- oombak_sim/src/message/request.rs
+++ oombak_sim/src/message/request.rs
@@ -15,6 +15,7 @@ pub struct Request {
 pub enum Payload {
     Run(usize),
     SetSignal(String, BitVec<u32>),
+    SetPeriodic(String, usize, BitVec<u32>, BitVec<u32>),
     Load(PathBuf),
     ModifyProbedPoints(ProbePointsModification),
     GetSimulationResult,
@@ -37,6 +38,17 @@ impl Request {
     pub fn set_signal(signal_name: String, value: BitVec<u32>) -> Message {
         let id = Self::random_id();
         let payload = Payload::SetSignal(signal_name, value);
+        Message::Request(Self { id, payload })
+    }
+
+    pub fn set_periodic(
+        signal_name: String,
+        period: usize,
+        low_value: BitVec<u32>,
+        high_value: BitVec<u32>,
+    ) -> Message {
+        let id = Self::random_id();
+        let payload = Payload::SetPeriodic(signal_name, period, low_value, high_value);
         Message::Request(Self { id, payload })
     }
 
@@ -75,6 +87,9 @@ impl std::fmt::Display for Payload {
         match self {
             Payload::Run(duration) => write!(f, "Run({duration})"),
             Payload::SetSignal(signal_name, _) => write!(f, "SetSignal({signal_name})"),
+            Payload::SetPeriodic(signal_name, period, _, _) => {
+                write!(f, "SetPeriodic({signal_name}, {period})")
+            }
             Payload::Load(path) => write!(f, "Load({})", path.to_str().unwrap()),
             Payload::ModifyProbedPoints(_) => write!(f, "ModifyProbedPoints"),
             Payload::GetSimulationResult => write!(f, "GetSimulationResult"),
