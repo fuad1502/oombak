6308f50159bbf103e4a037caab4a82c549de2265
Fuad Ismail <fuad1502@gmail.com>
2025-08-02 22:52:53 +0700

    oombak_rs: use rerun_except crate

--- oombak_rs/build.rs
+++ oombak_rs/build.rs
@@ -1,12 +1,16 @@
+use rerun_except::rerun_except;
+
 fn main() {
+    rerun_except(&["build/", "compile_commands.json", ".cache/", "target/"]).unwrap();
+
     let dst = cmake::Config::new("oombak_parser").build();
 
-    println!("cargo::rerun-if-changed=oombak_parser");
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
     println!(
         "cargo:rustc-link-search=native={}/build/_deps/slang-build/lib",
         dst.display()
     );
+
     println!("cargo:rustc-link-lib=static=oombak_parser");
     println!("cargo:rustc-link-lib=static=svlang");
     println!("cargo:rustc-link-lib=static=fmtd");
