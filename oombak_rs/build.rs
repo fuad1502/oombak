dc822857530b8970fb57a4171b639229e9cd65fc
Fuad Ismail <fuad1502@gmail.com>
2025-08-02 21:14:00 +0700

    create cargo workspace & build oombak_parser as part of oombak_rs

---
+++ oombak_rs/build.rs
@@ -0,0 +1,16 @@
+use cmake;
+
+fn main() {
+    let dst = cmake::Config::new("../oombak_parser/").build();
+
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!(
+        "cargo:rustc-link-search=native={}/build/_deps/slang-build/lib",
+        dst.display()
+    );
+    println!("cargo:rustc-link-lib=static=oombak_parser");
+    println!("cargo:rustc-link-lib=static=svlang");
+    println!("cargo:rustc-link-lib=static=fmtd");
+    println!("cargo:rustc-link-lib=static=mimalloc-debug");
+    println!("cargo:rustc-link-lib=dylib=stdc++");
+}
